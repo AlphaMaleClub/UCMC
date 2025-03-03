7273426534ba261e4018ba4c7d4d1fb8f84ea0b1
Philo-LEE <tlgus7777@gmail.com>
2025-03-03 14:59:36 +0900

    Config: initial

---
+++ src/main/java/com/alphamaleclub/ucmc/UcmcApplication.java
@@ -0,0 +1,13 @@
+package com.alphamaleclub.ucmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcmcApplication.class, args);
+    }
+
+}
